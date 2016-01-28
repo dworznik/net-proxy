0f96e9a1a3ecd4486eed062ab96c9cd8e11b40e9
Patryk Dwórznik <patryk@dworznik.com>
2016-01-29 01:07:47 +0900

    Add proxy

--- server.js
+++ server.js
@@ -1,10 +1,40 @@
 var express = require('express');
 var path = require('path');
 var webpack = require('webpack');
-var app = express();
+
+var netApi = require('net-browserify');
+var cors = require('cors');
+
+// Create proxy
+var proxy = express();
+
+proxy.use(function(req, res, next) {
+    var oldSetHeader = res.setHeader;
+    res.setHeader = function() {
+        if (!res._headerSent) {
+            oldSetHeader.apply(res, arguments);
+        } else {
+          console.log('Headers already sent.');
+        }
+    };
+    next();
+});
+proxy.use(cors({
+    credentials: true,
+    origin: 'http://localhost:8080',
+    methods: ['GET', 'POST', 'PUT', 'CONNECT', 'HEAD', 'OPTIONS']
+}));
+proxy.use(netApi());
+
+
+// Start the server 
+var server = proxy.listen(3000, function() {
+    console.log('Server listening on port ' + server.address().port);
+});
 
 var static_path = path.join(__dirname);
 
+var app = express();
 app.use(express.static(static_path))
   .get('/', function (req, res) {
     res.sendFile('index.html', {
